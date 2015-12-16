cc412daddbf7f7a1aa4f35fc6269a0d4d21b51be
Godsmith <filip.lange@gmail.com>
2015-12-16 21:53:59 +0100

    day16

--- day16/main.py
+++ day16/main.py
@@ -1,1 +1,31 @@
 __author__ = 'Filip'
+
+with open('input.txt') as f:
+    lines = f.readlines()
+
+ticker_tape = {
+    'children': 3,
+    'cats': 7,
+    'samoyeds': 2,
+    'pomeranians': 3,
+    'akitas': 0,
+    'vizslas': 0,
+    'goldfish': 5,
+    'trees': 3,
+    'cars': 2,
+    'perfumes': 1
+}
+
+for line in lines:
+    split_line = line.replace(':', '').replace(',', '').split(' ')
+    nr = split_line[1]
+    types = [split_line[i] for i in [2,4,6]]
+    amounts = [int(split_line[i]) for i in [3,5,7]]
+    for t in zip(types, amounts):
+        type = t[0]
+        amount = t[1]
+        if ticker_tape[type] != amount:
+            break
+    else:
+        print nr
+        break
